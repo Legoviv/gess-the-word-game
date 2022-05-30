23b2bf03962a45a96b10dd82b97310301f31830c
Legoviv <93427869+Legoviv@users.noreply.github.com>
2022-05-30 20:03:16 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -47,3 +47,45 @@ function send(){
     document.getElementById("word").value="";
 }
 
+question_turn="player1";
+answer_turn="player2";
+
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+
+    answer=get_answer.toLowerCase();
+    console.log("answer is "+answer);
+
+    if(answer==word){
+        if(answer_turn=="player1"){
+            player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else{
+            player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML = player2_score; 
+        }
+    }
+
+    if(question_turn=="player2"){
+        question_turn="player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+    else{
+        question_turn="player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+
+    if(answer_turn=="player2"){
+        answer_turn="player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+    else{
+        answer_turn="player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
+}
+
+
